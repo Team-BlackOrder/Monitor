e31b53365cbdae87a057d5ccf4e266632d9ece78
VJFernandez123 <vpfernando76@gmail.com>
2018-12-16 02:13:16 +0530

    Notification Service

--- notification-api/src/main/java/org/example/notification/api/Notification.java
+++ notification-api/src/main/java/org/example/notification/api/Notification.java
@@ -5,7 +5,7 @@ public class Notification {
     private String body;
 
     public Notification(){
-            System.out.print("fjdsojfbsafd");
+            System.out.print("fjdsojfbsafdsdfdf");
     }
 
     public Notification(String body){
